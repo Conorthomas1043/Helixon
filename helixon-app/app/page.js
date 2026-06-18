f80bea73504628f18f6c6fe92072f7ae23a2caef
Conor Thomas <conorthomas104@yahoo.com>
2026-06-18 16:56:14 +0100

    test: update homepage text

--- helixon-app/app/page.js
+++ helixon-app/app/page.js
@@ -3,6 +3,7 @@ export default function Home() {
     <main style={{ padding: 40, fontFamily: "sans-serif" }}>
       <h1>Helixon</h1>
       <p>The recruitment operating system.</p>
+      <p1>yoooooooo</p1>
     </main>
   );
 }
