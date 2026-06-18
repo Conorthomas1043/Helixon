42c68c54fd4ebae39a1420e51db15990f672ba26
Conor Thomas <conorthomas104@yahoo.com>
2026-06-19 00:22:44 +0100

    Add Sentry error tracking

---
+++ helixon-app/sentry.server.config.js
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1fa4e4a3f6cc154682501e16738d3cfb@o4511588978130944.ingest.de.sentry.io/4511588993073232",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
